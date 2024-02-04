6b8d77e80d722fdb7081b915808737e52d6cc593
Irina Kats <irina8kats@gmail.com>
2024-02-04 08:23:38 -0500

    feat: style active suggestions

--- src/components/Suggestions/Suggestions.tsx
+++ src/components/Suggestions/Suggestions.tsx
@@ -7,11 +7,13 @@ interface SuggestionsProps {
 
 export default function Suggestions(props: SuggestionsProps) {
   const { suggestionsList } = props;
+  const isActive = suggestionsList.length;
+  const activeClass = isActive ? 'suggestions_active' : '';
 
   return (
     <>
-      <div className="suggestions">
-        <h2 className="suggestions__title base-title">All suggestions</h2>
+      <div className={`suggestions ${activeClass}`}>
+        <h2 className="suggestions__title base-title">WittyWrite suggests</h2>
         <div className="suggestions__container">
           {suggestionsList.map(
             (suggestion, index) =>
